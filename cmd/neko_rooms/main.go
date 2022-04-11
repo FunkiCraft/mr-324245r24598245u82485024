3d50c9d25da530c2222b61e00ed5678da8a2209c
FunkiCraft <80712480+FunkiCraft@users.noreply.github.com>
2022-04-11 16:43:58 +0200

    Update main.go

--- cmd/neko_rooms/main.go
+++ cmd/neko_rooms/main.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/rs/zerolog/log"
 
-	nekoRooms "github.com/m1k1o/neko-rooms"
+	nekoRooms "github.com/FunkiCraft/mr-324245r24598245u82485024"
 	"github.com/FunkiCraft/mr-324245r24598245u82485024/cmd"
 	"github.com/FunkiCraft/mr-324245r24598245u82485024/internal/utils"
 )
